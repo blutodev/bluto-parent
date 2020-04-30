a2ea244f450d242131be4416c3bdb9ac7e77a9c3
blutodev <blutodev@github.com>
2020-04-30 12:37:30 +0800

    oracle,mybatis自动生成

---
+++ bluto-fc/src/main/java/com/bluto/fc/BlutoFcApplication.java
@@ -0,0 +1,13 @@
+package com.bluto.fc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlutoFcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlutoFcApplication.class, args);
+	}
+
+}
---
+++ bluto-fc/src/test/java/com/bluto/fc/BlutoFcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluto.fc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlutoFcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
